9fedc62c72fe5e330b4f700db856ab688ced4d56
Kishori Karale <kishori_k@techjoomla.com>
2022-10-10 13:26:05 +0530

    Task #189115 feat: com_subusers improvements

---
+++ src/administrator/sql/updates/mysql/1.1.0.sql
@@ -0,0 +1,10 @@
+
+ALTER TABLE `#__tjsu_roles` ENGINE = InnoDB;
+ALTER TABLE `#__tjsu_actions` ENGINE = InnoDB;
+ALTER TABLE `#__tjsu_role_action_map` ENGINE = InnoDB;
+ALTER TABLE `#__tjsu_users` ENGINE = InnoDB;
+
+ALTER TABLE `#__tjsu_roles` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `#__tjsu_actions` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `#__tjsu_role_action_map` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `#__tjsu_users` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
