dc6001af87769b5bfe0268f97bb3db19edca4a4d
Kishori Karale <kishori_k@techjoomla.com>
2022-11-04 17:39:41 +0530

    Task #189115 chore: Updated Indexing and table changes

--- src/administrator/sql/updates/mysql/1.1.0.sql
+++ src/administrator/sql/updates/mysql/1.1.0.sql
@@ -8,3 +8,6 @@ ALTER TABLE `#__tjsu_roles` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_uni
 ALTER TABLE `#__tjsu_actions` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE `#__tjsu_role_action_map` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE `#__tjsu_users` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE #__tjsu_users ADD INDEX user_id_idx (user_id);
+ALTER TABLE #__tjsu_users ADD INDEX client_id_idx (client_id);
